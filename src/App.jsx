2e833765f3acf930058312bec5972f1a9b31216a
DudeBruh326 <samuelpintado104@gmail.com>
2025-10-19 19:30:13 +0800

    Done creating and nesting components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+
+function App() {
+
+
+  return (
+
+    function MyButton() {
+    return (
+      <button>I'm a button</button>
+    );
+  }
+
+  );
+}
+
+export default App
