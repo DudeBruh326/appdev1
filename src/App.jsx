b7c23d194086ca6259f72344cbc7bf391ed8c7aa
DudeBruh326 <samuelpintado104@gmail.com>
2025-10-19 19:46:41 +0800

    Done rendering lists

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,37 @@ import './App.css'
 
 
 
-function AdminPanel(){
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+
   return (
-    <h1>Admin Panel</h1>
-  )
+    <ul>{listItems}</ul>
+  );
 }
 
-function LoginForm(){
-  return (
-    <h1>Login Form</h1>
-  )
-}
 
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  
 
   return (
     <>
-      <button onClick={()=>setIsLoggedIn(!isLoggedIn)}>click</button>
-      {isLoggedIn ? (
-        <AdminPanel />
-      ) : (
-        <LoginForm />
-      )}
-
+      <ShoppingList/>
     </>
   )
 }
