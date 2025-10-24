98ae4b821c8f121585f56b8e541d2e82243efd5f
DudeBruh326 <samuelpintado104@gmail.com>
2025-10-24 14:27:12 +0800

    Done with how to avoid confusing bugs by keeping components pure

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -151,3 +151,21 @@ export default function List() {
     </article>
   );
 }
+
+let guest = 0;
+
+function Cup() {
+  // Bad: changing a preexisting variable!
+  guest = guest + 1;
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaSet() {
+  return (
+    <>
+      <Cup />
+      <Cup />
+      <Cup />
+    </>
+  );
+}
