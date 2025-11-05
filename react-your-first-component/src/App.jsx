01e5ea13a88bb88e36b5abaabf386c3d32510ace
DudeBruh326 <samuelpintado104@gmail.com>
2025-11-05 19:57:59 +0800

    Done with using a component

--- react-your-first-component/src/App.jsx
+++ react-your-first-component/src/App.jsx
@@ -6,3 +6,13 @@ export default function Profile() {
 );
 }
 
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
