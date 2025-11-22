df0fc83f9c778a20aff131f0faac5278dc91df28
DudeBruh326 <samuelpintado104@gmail.com>
2025-11-22 14:26:21 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -9,3 +9,10 @@ export default function Avatar() {
     />
   );
 }
+
+export default function TodoList() {
+  const name = 'Gregorio Y. Zara';
+  return (
+    <h1>{name}'s To Do List</h1>
+  );
+}
